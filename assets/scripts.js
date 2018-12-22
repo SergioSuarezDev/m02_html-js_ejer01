1ede2cb3de44621aa917a7eb66e77db5b16de94e
Sergio Suarez <sergiosgil@gmail.com>
2018-12-23 00:17:50 +0100

    Se añade contenido, mas css y algo de js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,33 @@
 
+//Inicio de la App 
+ document.addEventListener('DOMContentLoaded', app)
+
+
 function app() {
+
+	var menu = document.querySelector('.menu');
+	var menutop = menu.offsetTop;
+		
+    window.addEventListener('scroll', fixMenu);
+    
+    //Funcion para fijar el menu arriba
+    function fixMenu(e) {
+		if(window.scrollY >= menutop) {
+			menu.classList.add('fixed');
+		} else {
+			menu.classList.remove('fixed');
+		}
+	}
+	
 
     //Funcion para hacer el Smooth Scroll
     document.querySelectorAll('a[href~="#"]').forEach(enlace => { //Por cada link con el href y el #
-        const url = new URL(enlace.href); //Creo el objeto url con el enlace
-        enlace.addEventListener('click', () => { //En cada click detectado
-            document.getElementById(url.hash.substr(1)).scrollIntoView({behavior: 'smooth'}); //Nos movemos al ID
-        });
+            const url = new URL(enlace.href); //Creo el objeto url con el enlace
+            enlace.addEventListener('click', () => { //En cada click detectado
+               if (url.hash.length > 0) { //Si tiene 1 o mas caracter despues del #
+                document.getElementById(url.hash.substr(1)).scrollIntoView({behavior: 'smooth'}); //Nos movemos al ID
+            }
+            });
     });
 
 
@@ -15,6 +36,5 @@ function app() {
 
 
 
-document.addEventListener('DOMContentLoaded', app)
 
 
