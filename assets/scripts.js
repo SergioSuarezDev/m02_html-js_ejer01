e4aa1c4446fbce25aa9ba8f31f721c281611aa1c
Sergio Suarez <sergiosgil@gmail.com>
2018-12-27 14:36:20 +0100

    Mejoradas las validaciones  en html5 y javascript

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,13 +4,21 @@
 
 function app() {
     
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0); //Que inicie siempre con el scroll arriba
 
+    //Selectores para la gestión del menu
     let menu = document.querySelector('.menu');
     let menuMobile = document.querySelector('.menuMobile');
     let menutop = menu.offsetTop;
     let iconoMobile = document.getElementById('iconMenu');
 
+    //Select para la opcion de "otros" en el select del formulario
+    let select = document.querySelector('select[name="selector"]');
+
+    //Event para fijar el menu arriba y el ancho (menu)
+    window.addEventListener('scroll', fixMenu);
+
+    //Click en el icono del menu Mobile
     iconoMobile.onclick = function() {
 
       if (menu.classList.contains('MenuMobile')) {
@@ -39,7 +47,8 @@ function app() {
       }
     };
 
-    let select = document.querySelector('select[name="selector"]');
+
+    //Cuando cambie el menu si esta seleccionado el "otro" creamos un input nuevo
     select.addEventListener('change',function(e){
        if (e.target.value == "otro") {
             let br = document.createElement("br");
@@ -53,50 +62,56 @@ function app() {
     });
 
 
-    //Event para fijar el menu arriba y el ancho (menu)
-    window.addEventListener('scroll', fixMenu);
+    //Cuando se envie el formulario, hago todas las validaciones
+    //Ya se que se peuden hacer con el pattern, pero por si acaso
+    //las valido todas de nuevo aqui tambien
     document.querySelector('form').addEventListener('submit', evento => {
 
+        //Selectores para los elementos del form
         let emailV = document.querySelector('input[name="email"]');
         let tlfV = document.querySelector('input[name="telefono"]');
         let palabras  = document.getElementById("textComment").value; 
         let numeroP = palabras.split(' ');
+        let errorSpan = document.querySelector('.error');
 
+        //Error para mas de 150 palabras en textarea
         if(numeroP.length > 150) {
-            alert("No se pemiten mas de 150 palabras");
+            errorSpan.innerHTML = 'Error, no se permiten mas de 150 palabras, por favor revise el contenido.';
             evento.preventDefault();
             return
          }
 
+         //Error para el email no valido
          if(!validaEmail(emailV.value)){
-             alert("Email no válido");
+             errorSpan.innerHTML = 'Error en email, por favor revise el contenido.';
              evento.preventDefault();
              return
          }
 
+         //Error para el telefono no valido
          if(!validaNumero(tlfV.value)){
-            alert("Numero no válido");
+            errorSpan.innerHTML = 'Error en el telefono, por favor revise el contenido.';
             evento.preventDefault();
             return
         }
 
-        //Si llegamos hasta aqui todo es correo
-        alert("Correo enviado");
-        evento.preventDefault();
+        //Si llegamos hasta aqui todo es correcto
+        errorSpan.innerHTML = ''; //Borro el error si lo hubiese
+        evento.preventDefault(); //No lo enviamos, no hay nada en el servidor que pueda hacerlo
 
-        });
+    });
 
-        //validamos el email
-        function validaEmail(email) {
-            let reg = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-            return reg.test(email);
-        }
+        //validamos el email tambien por js
+    function validaEmail(email) {
+        let reg = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return reg.test(email);
+    }
 
-        //Validamos el tlf
-        function validaNumero(numero) {
-            let reg = /^\d+$/;
-            return reg.test(numero);
-          }
+        //Validamos el tlf tambien por js
+    function validaNumero(numero) {
+        let reg = /[0-9]{9}/;
+        return reg.test(numero);
+    }
 
     //Funcion para fijar el menu arriba (Ojo si tiene el menu mobile no lo hago)
     function fixMenu(e) {
@@ -108,7 +123,6 @@ function app() {
 			menu.classList.remove('fixed');
 		}
 	}
-    
 
     //Funcion para hacer el Smooth Scroll
     document.querySelectorAll('a[href~="#"]').forEach(enlace => { //Por cada link con el href y el #
